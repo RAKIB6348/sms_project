88edb1ba88c0558443bd03bfe7654883b3f2f860
RAKIB6348 <shahidulhuda0@gmail.com>
2026-07-27 22:40:34 +0600

    update admin file

--- auth_user/admin.py
+++ auth_user/admin.py
@@ -8,7 +8,7 @@ from .models import CustomUser
 class CustomUserAdmin(UserAdmin):
     list_display = ('username', 'email', 'user_type', 'registration_no', 'is_staff')
     list_filter = ('user_type', 'is_staff', 'is_superuser')
-    search_fields = ('username', 'email', 'user_type')
+    search_fields = ('username', 'email', 'user_type','registration_no')
     fieldsets = UserAdmin.fieldsets + (
         ('Custom Fields', {'fields': ('user_type', 'profile_pic')}),
     )
