fc6b75362b55b4ac66bb8b603aa9aec1ae5a6383
RAKIB6348 <shahidulhuda0@gmail.com>
2026-07-27 22:07:25 +0600

    user model register into admin file

--- auth_user/admin.py
+++ auth_user/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import CustomUser
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'user_type', 'is_staff')
+    list_filter = ('user_type', 'is_staff', 'is_superuser')
+    fieldsets = UserAdmin.fieldsets + (
+        ('Custom Fields', {'fields': ('user_type', 'profile_pic')}),
+    )
