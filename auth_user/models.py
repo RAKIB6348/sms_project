0bb45e2e7f64df744b9bdb12b69fcc001d3a9bff
RAKIB6348 <shahidulhuda0@gmail.com>
2026-07-27 22:05:19 +0600

    create customuser model

--- auth_user/models.py
+++ auth_user/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    USER_TYPE_CHOICES = (
+        ('admin', 'Admin'),
+        ('teacher', 'Teacher'),
+        ('student', 'Student'),
+    )
+    user_type = models.CharField(max_length=20, choices=USER_TYPE_CHOICES)
+    profile_pic = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.username} ({self.get_user_type_display()})"
