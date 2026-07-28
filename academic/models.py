f70e73cc58fc2c1e5b37436601a1c34e233cb646
RAKIB6348 <shahidulhuda0@gmail.com>
2026-07-28 11:17:00 +0600

    create new academic model

--- academic/models.py
+++ academic/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class AcademicYear(models.Model):
+    year = models.CharField(max_length=10)
+    is_active = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.year
+    
+
+
+class Section(models.Model):
+    name = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.school_class.name} - {self.name}"
