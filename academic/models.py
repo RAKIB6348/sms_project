e50b3e1d49043a66721eea8e3c40639c0508ccb6
RAKIB6348 <shahidulhuda0@gmail.com>
2026-07-28 11:25:39 +0600

    create subject models

--- academic/models.py
+++ academic/models.py
@@ -19,3 +19,14 @@ class Section(models.Model):
 
     def __str__(self):
         return f"{self.name}"
+    
+
+
+class Subject(models.Model):
+    name = models.CharField(max_length=100)
+    code = models.CharField(max_length=20, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.name} ({self.code})"
