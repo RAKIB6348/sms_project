73d26b3a6c37a446087a59560984d779f1953052
RAKIB6348 <shahidulhuda0@gmail.com>
2026-09-09 12:37:38 +0600

    add subject page and urls and views

--- academic/urls.py
+++ academic/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from academic.views import *
+
+from . import views
 
 urlpatterns = [
-    
+    path('subjects/', views.subject_list, name='subject_list'),
+    path('subjects/add/', views.subject_add, name='subject_add'),
 ]
--- academic/views.py
+++ academic/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import redirect, render
 
-# Create your views here.
+from .models import Subject
+
+
+# TODO: Create subject list template and implement full list view
+def subject_list(request):
+    subjects = Subject.objects.all()
+    return render(request, 'academic/subject/subject_list.html', {'subjects': subjects})
+
+
+def subject_add(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        code = request.POST.get('code')
+
+        if Subject.objects.filter(code=code).exists():
+            messages.error(request, 'Subject code already exists.')
+            return render(request, 'academic/subject/add-subject.html', {'form_data': {'name': name, 'code': code}})
+
+        Subject.objects.create(name=name, code=code)
+        messages.success(request, 'Subject added successfully.')
+        return redirect('subject_list')
+
+    return render(request, 'academic/subject/add-subject.html')
